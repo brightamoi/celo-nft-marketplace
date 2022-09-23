c74ca7e1b6349734030a247c0a1dbc35ca92b991
brightamoi <amoi.bright92@gmail.com>
2022-09-23 12:49:58 -0400

    frontend added

---
+++ frontend/constants.js
@@ -0,0 +1,4 @@
+export const SUBGRAPH_URL =
+  "https://api.thegraph.com/subgraphs/name/brightamoi/celo-nft-marketplace";
+
+export const MARKETPLACE_ADDRESS = "0xE797fc78C033bd3D44e6d7e8593E034959F06fC6";
